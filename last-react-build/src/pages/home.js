66ac6ae7060280ab4abf3b5acf69c2881d667ee2
Duyle123 <ddl8997@nyu.edu>
2023-04-05 14:46:19 -0400

    commit

--- last-react-build/src/pages/home.js
+++ last-react-build/src/pages/home.js
@@ -1,36 +1,36 @@
-//========= Import Bootstrap ==============
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import {gsap, Power3} from 'gsap';
+// //========= Import Bootstrap ==============
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import {gsap, Power3} from 'gsap';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-import '../App.css';
-import React, { useRef } from 'react';
+// import '../App.css';
+// import React, { useRef } from 'react';
 
-function HomePage() {
-    let slideEl = useRef(null);
+// function HomePage() {
+//     let slideEl = useRef(null);
 
-    return (
-        <Container fluid className='hero justify-content-center strawford-bold'>
-            <Row lg={8} fluid>
-                <Col sm={1} className='hero-container-1'>hello</Col>
-                <Col sm={1} className='hero-container-1'>put info here</Col>
-            </Row>
-            <Row lg={8} fluid className='justify-content-center'>
-                <Col lg={7} className='justify-content-center hero-container-1'>
-                    <Row ref={el => slideEl = el} md={7} className=' title ch2 strawford-bold'> DUY'S PORTFOLIO </Row>
-                    <Row ref={el => slideEl = el} md='auto' className='ch3'> <a className='strawford-light l-link' href="">Projects</a> </Row>
-                    <Row ref={el => slideEl = el} md='auto' className='ch3'> <a className='strawford-light l-link' href="">Writing Samples</a> </Row>
-                    <Row ref={el => slideEl = el} md='auto' className='ch3'> <a className='strawford-light l-link' href="">Contact</a> </Row>
-                </Col>
-                <Col lg={3} className="hero-container-2">
+//     return (
+//         <Container fluid className='hero justify-content-center strawford-bold'>
+//             <Row lg={8} fluid>
+//                 <Col sm={1} className='hero-container-1'>hello</Col>
+//                 <Col sm={1} className='hero-container-1'>put info here</Col>
+//             </Row>
+//             <Row lg={8} fluid className='justify-content-center'>
+//                 <Col lg={7} className='justify-content-center hero-container-1'>
+//                     <Row ref={el => slideEl = el} md={7} className=' title ch2 strawford-bold'> DUY'S PORTFOLIO </Row>
+//                     <Row ref={el => slideEl = el} md='auto' className='ch3'> <a className='strawford-light l-link' href="">Projects</a> </Row>
+//                     <Row ref={el => slideEl = el} md='auto' className='ch3'> <a className='strawford-light l-link' href="">Writing Samples</a> </Row>
+//                     <Row ref={el => slideEl = el} md='auto' className='ch3'> <a className='strawford-light l-link' href="">Contact</a> </Row>
+//                 </Col>
+//                 <Col lg={3} className="hero-container-2">
 
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+//                 </Col>
+//             </Row>
+//         </Container>
+//     );
+// }
 
-export default HomePage;
+// export default HomePage;
--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,32 +1,13 @@
 //========= Import Bootstrap ==============
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import {gsap, Power3} from 'gsap';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
 
 function HomePage() {
     return (
-        <Container fluid className='hero justify-content-center strawford-bold'>
-            <Row lg={8} fluid>
-                <Col sm={1} className='hero-container-1'>hello</Col>
-                <Col sm={1} className='hero-container-1'>put info here</Col>
-            </Row>
-            <Row lg={8} fluid className='justify-content-center'>
-                <Col lg={7} className='justify-content-center hero-container-1'>
-                    <Row md={7} className=' title ch2 strawford-bold'> DUY PORTFOLIO </Row>
-                    <Row md='auto' className='ch3'> <a className='strawford-light l-link' href="">Projects</a> </Row>
-                    <Row md='auto' className='ch3'> <a className='strawford-light l-link' href="">Writing Samples</a> </Row>
-                    <Row md='auto' className='ch3'> <a className='strawford-light l-link' href="">Contact</a> </Row>
-                </Col>
-                <Col lg={3} className="hero-container-2">
-
-                </Col>
-            </Row>
-        </Container>
+        <div className="home">
+            homepage
+        </div>
     );
 }
 
