fcefac9c69c2e9bedfcadc22ddc83d5856beb2fd
Duyle123 <ddl8997@nyu.edu>
2023-04-23 22:16:13 -0400

    commit

--- next.config.js
+++ next.config.js
@@ -3,13 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-
+  webpack5: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     })
+    config.resolve.fallback = { fs: false };
 
     return config;
   },
