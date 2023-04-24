805eddeb93a3ed837ff320d7dd6d94d9023d34af
Duyle123 <ddl8997@nyu.edu>
2023-04-23 22:22:06 -0400

    commit

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -15,9 +15,11 @@ export function generateStaticParams() {
     }))
 }
 
-export async function generateMetaData({params}: {params: { postId: string }}) {
-    const posts = getSortedPostsData()
-    const {postId} = params;
+export function generateMetadata({ params }: { params: { postId: string } }) {
+
+    const posts = getSortedPostsData() //deduped!
+    const { postId } = params
+
     const post = posts.find(post => post.id === postId)
 
     if (!post) {
@@ -25,10 +27,12 @@ export async function generateMetaData({params}: {params: { postId: string }}) {
             title: 'Post Not Found'
         }
     }
+
     return {
         title: post.title,
     }
 }
+
 
 export default async function Post({ params }: { params: { postId: string } }) {
     const posts = getSortedPostsData() //deduped!
