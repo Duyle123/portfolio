b4cc2810ef2a6c99086be815b9f8b92b94f63f5d
Duyle123 <ddl8997@nyu.edu>
2023-05-23 13:36:10 -0400

    commit

--- src/components/user-info/ProjectInfo.tsx
+++ src/components/user-info/ProjectInfo.tsx
@@ -1,10 +1,11 @@
 import teraImage from "../../media/images/terathumb.png";
 import parkNYCImage from "../../media/images/parkNYCImage.png";
+import netflixImage from "../../media/images/netflix-mass-media.png";
 
 const ProjectInfo = [{
         id: '1',
         title: 'Tera Group Designs',
-        role: 'Strategy, Copy, Design',
+        role: 'Strategy; Copy; Design',
         summary: "As a digital marketing intern at Tera Group / VTVcab, I've led initiatives in web and graphic design for one of its projects. I've also managed content creation on all of their platforms.",
         thumbnail: teraImage,
         thumbnailAlt:'Tera Group Co. Ltd. Technology Company with 3-d made logo, Slogan says Tera Cells Make The Universe. Made by Duy Le and Dong Luong',
@@ -24,6 +25,19 @@ const ProjectInfo = [{
         secondaryColor: '#BD8C2D',
         shadowColor: '#CAE2C6',
         link: '/projects/DMTP_climate_change_project/index.html'
+    },
+    {
+        id: '3',
+        title: 'Netflix: A Mass Media Machine',
+        role: 'Design; Code; Copy',
+        summary: '',
+        thumbnail: netflixImage,
+        thumbnailAlt:'Netflix - A Mass Media Machine. A Project by Duy Le',
+        primaryColor: '#E50914',
+        secondaryColor: '#B1060F',
+        shadowColor: '#E39DA1',
+        link: '/projects/culture_industries/index.html'
     }
 ]
+
 export default ProjectInfo;
