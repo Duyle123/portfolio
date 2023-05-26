3c10cad315b6c6894dcd1361283133b0fe933de2
Duy Le <ddl8997@nyu.edu>
2023-05-25 23:57:01 -0400

    commit

--- src/pages/tera.js
+++ src/pages/tera.js
@@ -1,5 +1,9 @@
 import Header from "../components/interactive/header";
+import Image from "next/image";
 import '../app/globals.css';
+import '../media/images/parkNYCImage.png'
+
+import HeroImage from '../media/images/five-broken-cameras/hug-tree.jpg';
 
 function TeraPage () {
     return (
@@ -7,8 +11,20 @@ function TeraPage () {
             <Header />
             <div>
                 <h1 className="">Tera Group Projects</h1>
+                <Image 
+                alt=''
+                src={HeroImage}
+                className="
+                w-[100vw] h-[80vh] 
+                object-cover"
+                >
+
+                </Image>
                 <p>
                     This is a page for Tera.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ea necessitatibus incidunt ipsam nemo perferendis assumenda expedita autem esse deserunt nam omnis, in blanditiis cumque ad praesentium, voluptatum laborum amet.
                 </p>
             </div>
         </>
