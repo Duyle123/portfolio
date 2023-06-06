c88c32a8b6ca9a6bf413b80d4ab6ab6d5ed5fe85
Duy Le <ddl8997@nyu.edu>
2023-06-06 13:53:46 -0400

    commit

--- src/components/interactive/header.tsx
+++ src/components/interactive/header.tsx
@@ -3,7 +3,9 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <header className='container mx-auto'>
-      <nav className='flex flex-row gap-[50px] 
+      <nav className='
+      border
+      flex flex-row gap-[50px] 
       pt-5 pb-5'>        
         <Link href="/">
               Home
