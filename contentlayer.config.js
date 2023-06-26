a0839d0ff3914386b66563f4835d8b4d5822ceca
Duy Le <ddl8997@nyu.edu>
2023-06-26 17:50:07 -0400

    commit

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,5 +1,7 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files'
-import { m } from 'framer-motion'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypePrettyCode from 'rehype-pretty-code'
+import rehypeSlug from 'rehype-slug'
 import remarkGfm from 'remark-gfm'
 
 const docResolve = {
